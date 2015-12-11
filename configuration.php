087d160eab8e152741300d43b6fa2e8bc0149618
Binny V A <binnyva@gmail.com>
2015-12-11 14:00:25 +0530

    Better iFrame intergration

--- configuration.php
+++ configuration.php
@@ -1,7 +1,6 @@
 <?php
 //Configuration file for iFrame
 // Leave Empty - file extistance is needed for accurate directory matching.
-$config = $config_data + array(
-	'site_title'	=> 'Donut LeaderBoard',
-);
+$config = $config_data;
+$config['site_title'] = 'Donut LeaderBoard';
 $config['site_home'] = $config_data['site_home'] . 'apps/donutleaderboard/';
